560dc28386a1cde4d9cbe315fd2fd99fb7d13fa2
Christopher <christopher.carnivale@gmail.com>
2019-02-07 13:31:07 -0500

    Lunch break

--- src/List.js
+++ src/List.js
@@ -3,17 +3,20 @@ import Card from './Card'
 import './List.css';
 
 export default function List(props) {
+  console.log(props.cards)
   return (
     <section className='List'>
       <header className='List-header'>
         <h2>{props.header}</h2>
       </header>
       <div className='List-cards'>
-        {props.cards.map((card) =>
+        {props.cards.map(card =>
           <Card
             key={card.id}
             title={card.title}
             content={card.content}
+            handleDelete={props.handleDelete}
+            id={card.id}
           />
         )}
         <button
